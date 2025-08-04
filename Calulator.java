ce0e05b5e6fb61e6818f330fc72078faa73b64a1
Sana Tasneem Azimudin <sanaazimudin432@gmail.com>
2025-08-04 23:57:06 +0530

    Update Calulator.java

--- Calulator.java
+++ Calulator.java
@@ -3,13 +3,13 @@ public class Calculator
 {
     public static void main(String[] args) 
     {
-        Scanner sc = new Scanner(System.in);
+        Scanner s = new Scanner(System.in);
         System.out.print("Enter first number: ");
-        double num1 = sc.nextDouble();
+        double num1 = s.nextDouble();
         System.out.print("Enter second number: ");
-        double num2 = sc.nextDouble();
+        double num2 = s.nextDouble();
         System.out.print("Choose an operator (+, -, *, /): ");
-        char operator = sc.next().charAt(0);
+        char operator = s.next().charAt(0);
         double result;
         switch (operator) 
         {
@@ -41,3 +41,4 @@ public class Calculator
         }
     }
 }
+
