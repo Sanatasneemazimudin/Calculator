05f9e588eb22242e880aa39cb783ac05b5565521
Sana Tasneem Azimudin <sanaazimudin432@gmail.com>
2025-08-27 17:08:11 +0530

    Add files via upload

---
+++ WordCounter.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class WordCounter {
+    public static void main(String[] args) {
+        System.out.println("Sana Tasneem Azimudin-2024503007");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string:");
+        String input = sc.nextLine();
+        if (input == null || input.trim().isEmpty()) {
+            System.out.println("Number of words: 0");
+        } else {
+            String[] words = input.trim().split(" ");
+            int wordCount = words.length;
+            System.out.println("Number of words: " + wordCount);
+        }
+    }
+}
