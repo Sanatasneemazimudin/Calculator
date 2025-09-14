9692058116dc493616353b8694464a0f75976ec3
Sana Tasneem Azimudin <sanaazimudin432@gmail.com>
2025-09-15 01:40:01 +0530

    Add files via upload

---
+++ BMI.java
@@ -0,0 +1,28 @@
+import java.lang.*;
+import java.util.Scanner;
+class BMI
+{
+      public static void main(String[] args)
+      { 
+             System.out.println("Sana Tasneem Azimudin-2024503007");
+             Scanner sc=new Scanner(System.in);
+             double kgs,mtrs;
+             System.out.println("Enter your weight(in kgs) and height(in meters):");
+             kgs=sc.nextDouble();
+             mtrs=sc.nextDouble();
+             double bmi=kgs/(mtrs*mtrs);
+             if(18.5<=bmi&&bmi<25)
+             {
+               System.out.println("Your Body mass index is normal.");
+               System.out.println("Weight:"+kgs+"kgs");
+               System.out.println("Height:"+mtrs+"meters");
+             }
+             else
+             { 
+                 System.out.println("Your Body mass index is not normal.");
+                 System.out.println("Weight:"+kgs+"kgs");
+                 System.out.println("Height:"+mtrs+"meters");
+             }
+      }
+}  
+     
