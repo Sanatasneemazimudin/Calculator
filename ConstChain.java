ffbf3c115b09faf3c036156d139adebc5ce751d2
Sana Tasneem Azimudin <sanaazimudin432@gmail.com>
2025-09-17 21:12:05 +0530

    Add files via upload

---
+++ ConstChain.java
@@ -0,0 +1,21 @@
+class ConstChain {
+    int balan, acno, widr;
+    ConstChain() {
+        this(1000, 5185, 500);
+}
+    ConstChain(int bal, int accno, int withdr) {
+        balan = bal;
+        acno = accno;
+        widr = withdr;
+}
+    void display() {
+        System.out.println("Account Number: " + acno);
+        System.out.println("Balance: " + balan);
+        System.out.println("Withdraw: " + widr);
+}
+    public static void main(String[] args) {
+         System.out.println("Sana Tasneem Azimudin-2024503007");
+        ConstChain obj = new ConstChain();
+        obj.display();
+}
+}
