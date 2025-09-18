26a8548ffd98306956dda0f2467af7ba7c2511f3
Sana Tasneem Azimudin <sanaazimudin432@gmail.com>
2025-09-19 00:36:51 +0530

    Add files via upload

---
+++ Block.java
@@ -0,0 +1,16 @@
+class Block {
+    static int bankno;
+    static {
+        bankno = 890;
+        System.out.println("The bank number is " + bankno);
+}
+    static void staticdisplay() {
+        System.out.println("Bank Number: " + bankno);
+}
+    void display() {
+        System.out.println("Bank Number: " + bankno);
+}
+    public static void main(String[] args) {
+        System.out.println("Sana Tasneem Azimudin-2024503007");
+        staticdisplay();}
+}
