c9c3a49006681c1494eb3ddd15f664c29e4aecf9
Sana Tasneem Azimudin <sanaazimudin432@gmail.com>
2025-10-23 16:33:04 +0530

    Add files via upload

---
+++ Obj.java
@@ -0,0 +1,11 @@
+class Obj {
+    void display() {
+        System.out.println("Bank object");
+}
+
+    public static void main(String args[]) {
+        System.out.println("Sana Tasneem Azimudin-2024503007");
+        Obj obj = new Obj();
+        obj.display();
+}
+}
