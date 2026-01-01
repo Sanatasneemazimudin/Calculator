dac47bee906034ce9390313b0879732c6fd914a8
Sana Tasneem Azimudin <sanaazimudin432@gmail.com>
2026-01-02 02:01:54 +0530

    Update ConstChain.java

--- ConstChain.java
+++ ConstChain.java
@@ -14,9 +14,10 @@ class ConstChain {
         System.out.println("Withdraw: " + widr);
 }
     public static void main(String[] args) {
-         System.out.println("Sana Tasneem Azimudin-2024503007");
+         System.out.println("Sana Tasneem Azimudin=2024503007");
         ConstChain ob = new ConstChain();
         ob.display();
 }
 
 }
+
