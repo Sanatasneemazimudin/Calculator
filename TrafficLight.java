01ab5840d4d97382e6dfcbed109d9ef38ece1cb4
Sana Tasneem Azimudin <sanaazimudin432@gmail.com>
2026-01-02 00:28:39 +0530

    Add files via upload

---
+++ TrafficLight.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class TrafficLight 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Sana Tasneem Azimudin-2024503007");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the color of the traffic light (red, yellow, or green): ");
+        String input = scanner.nextLine().toLowerCase();
+        switch (input) 
+        {
+            case "red":
+                System.out.println("Stop");
+                break;
+            case "yellow":
+                System.out.println("Ready to move");
+                break;
+            case "green":
+                System.out.println("Go");
+                break;
+            default:
+                System.out.println("Invalid color");
+                break;
+}
+}
+}
