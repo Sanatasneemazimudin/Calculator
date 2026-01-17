7d8ccfcff5a32971707c3edf4868119bb87bf1cc
Sana Tasneem Azimudin <sanaazimudin432@gmail.com>
2026-01-18 03:00:15 +0530

    Refactor Block class formatting and output

--- Block.java
+++ Block.java
@@ -11,9 +11,10 @@ class Block {
         System.out.println("Bank Number: " + bankno);
 }
     public static void main(String[] args) {
-        System.out.println("Sana Tasneem Azimudin=2024503007");
+        System.out.println("Sana Tasneem Azimudin-2024503007");
         staticdisplay();}
 
 }
+
 
 
