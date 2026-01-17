e57339611ac0a8586e55ac0fd97ad1cb5d41a1f9
Sana Tasneem Azimudin <sanaazimudin432@gmail.com>
2026-01-17 15:23:29 +0530

    Update Block.java

--- Block.java
+++ Block.java
@@ -11,8 +11,9 @@ class Block {
         System.out.println("Bank Number: " + bankno);
 }
     public static void main(String[] args) {
-        System.out.println("Sana Tasneem Azimudin-2024503007");
+        System.out.println("Sana Tasneem Azimudin=2024503007");
         staticdisplay();}
 
 }
+
 
